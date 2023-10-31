df0226090bb141a2613a544c5ba081cec7eddd15
Ulises Farias <Ulisesfarias1905@hotmail.com>
2023-10-31 10:17:08 -0300

    kitchening

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+/* requires */
 const express = require('express');
 const path = require('path');
+
+/* settings */
 const app = express();
-
-const PORT = 3000;
-
-app.use(express.static(path.join(__dirname,'public')));
+const PORT = 3030;
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+/* routes */
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 
 
-
-
-app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))
+/* server */
+app.listen(PORT, () => console.log('Server running in http://localhost:' + PORT))
