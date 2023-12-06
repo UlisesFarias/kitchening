94afbe6cec608fcf7245164eb419a99a2674b738
Ulises Farias <Ulisesfarias1905@hotmail.com>
2023-12-06 14:18:37 -0300

    kitchening

--- src/app.js
+++ src/app.js
@@ -4,25 +4,33 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index.routes');
+const indexRouter = require('./routes/index.routes')
 const usersRouter = require('./routes/users.routes');
-const productsRouter = require('./routes/products.routes')
+const productsRouter = require('./routes/products.routes');
+
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'..', 'public')));
+app
+  .use(logger('dev'))
+  .use(cookieParser())
 
-app.use('/', indexRouter);
-app.use('/usuarios', usersRouter);
-app.use('/productos',productsRouter)
+  /* formularios */
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  
+  /* recursos estaticos */
+  .use(express.static(path.join(__dirname,'..', 'public')))
+
+  /* rutas */
+  .use('/', indexRouter)
+  .use('/usuarios', usersRouter)
+  .use('/productos', productsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
